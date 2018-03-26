35ff610f891123b12fa51e0d6333116c014937ce
milan.kerslager <milan.kerslager@W10100.sps-cl.local>
2018-03-26 12:22:04 +0200

    První commit

---
+++ src/nazdar/světe/NazdarSvěte.java
@@ -0,0 +1,19 @@
+package nazdar.světe;
+
+/**
+ *
+ * @author Milan Keršláger
+ */
+public class NazdarSvěte {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Nazdar bazar!");
+
+        for (int i = 0; i < 10; i++) {
+            System.out.println(i + " ");
+        }
+    }
+}
